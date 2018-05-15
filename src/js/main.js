5abfc7ef759391d024db0e172f0e8fdb454501c9
Rukio <39274302+Rukio@users.noreply.github.com>
2018-05-15 23:17:44 +0300

    Optimised a bit

--- src/js/main.js
+++ src/js/main.js
@@ -5,15 +5,19 @@ var mainBlock,
 
 window.onload = function() {
 
-    mainBlock = document.querySelector('.main-block'),
-    blockLayer = document.querySelectorAll('.main-block__layer'),
+    mainBlock   = document.querySelector('.main-block'),
+    blockLayer  = document.querySelectorAll('.main-block__layer'),
+    scrollStep  = 20,
+    blocksTotal = blockLayer.length - 1;
+    blockWidth  = 400;
 
     mainBlock.onmousewheel = function(event) {
         console.log(event);
         event = event || window.event;
-        if (layer < 2 & event.deltaY == 100) {
+
+        if (layer < blocksTotal & event.deltaY == 100) {
             if (layerY > -400) {
-                layerY -= 10;
+                layerY -= scrollStep;
             } else
             {
                 layer++;
@@ -24,7 +28,7 @@ window.onload = function() {
 
         if (layer >= 0 & event.deltaY == -100) {
             if (layerY < 0) {
-                layerY += 10;
+                layerY += scrollStep;
             } else
             {
                 if (layer > 0) {
